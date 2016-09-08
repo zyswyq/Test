e669bf11136efc6a566d38318cedfd4a481f35d1
wyqzys <763958570@qq.com>
2016-09-08 10:30:47 +0800

    111

--- MyApplication/app/src/main/java/com/wang/firstgit/MainActivity.java
+++ MyApplication/app/src/main/java/com/wang/firstgit/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //功能1
+        //功能132323232
     }
 }
